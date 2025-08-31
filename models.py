5fb1276cbadcf51447ba716171ede5a7b2f6e06e
Bruce Garro <brucegarro@Bruces-MacBook-Pro.local>
2025-08-31 14:51:32 -0400

    Change Text fields to String

--- models.py
+++ models.py
@@ -1,16 +1,17 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Text, Date, DateTime, PrimaryKeyConstraint, func
+from sqlalchemy import String, Date, DateTime, PrimaryKeyConstraint, UniqueConstraint, func
 
 
 class Base(DeclarativeBase):
     pass
 
+
 class SeenEvent(Base):
     __tablename__ = "seen_events"
 
-    user_id: Mapped[str] = mapped_column(Text, nullable=False)
+    user_id: Mapped[str] = mapped_column(String, nullable=False)
     date: Mapped[object] = mapped_column(Date, nullable=False)
-    endpoint: Mapped[str] = mapped_column(Text, nullable=False)
+    endpoint: Mapped[str] = mapped_column(String, nullable=False)
     first_seen: Mapped[object] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now(),
